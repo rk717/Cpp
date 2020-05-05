aa81e8dca801c36949a61e0238c6b4923e1b4077
rk717 <55735239+rk717@users.noreply.github.com>
2020-05-05 15:56:49 +0200

    Update and rename 移除相同的数字vector.cpp to vector操作.cpp

--- notes/vector操作.cpp
+++ notes/vector操作.cpp
@@ -1,6 +1,7 @@
 用erase 来 给vector 去除重复
 
-fac2.erase(unique(fac2.begin(), fac2.end()), fac2.end());
+sort( vec.begin(), vec.end() );
+vec.erase( unique( vec.begin(), vec.end() ), vec.end() );
 
 ///////////////////////////////////////////////////////////////////
 
