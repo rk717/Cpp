39cbe01841022a5493e058d0f259c99a31000119
rk717 <55735239+rk717@users.noreply.github.com>
2020-05-05 13:49:48 +0200

    Update 移除相同的数字vector.cpp

--- notes/移除相同的数字vector.cpp
+++ notes/移除相同的数字vector.cpp
@@ -1,3 +1,9 @@
 用erase 来 给vector 去除重复
 
 fac2.erase(unique(fac2.begin(), fac2.end()), fac2.end());
+
+///////////////////////////////////////////////////////////////////
+
+找vector里的最大值
+//finding the maximum element
+    int max = *max_element(v1.begin(), v1.end());
